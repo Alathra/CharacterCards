12d07fa354530678408e9f81395de3026af89297
Schbem <131344591+Schbem@users.noreply.github.com>
2025-04-14 18:43:39 +0800

    perf: refactored Cards.java

--- src/main/java/io/github/alathra/charactercards/core/Cards.java
+++ src/main/java/io/github/alathra/charactercards/core/Cards.java
@@ -11,113 +11,108 @@ import org.bukkit.entity.Player;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Cards {
+    private static final ZoneId DEFAULT_ZONE = ZoneId.of("America/New_York");
+    private static final DateTimeFormatter DEFAULT_TIME_FORMAT = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm:ss a z");
+
+    /**
+     * Displays the character card of an online player to a player, showing all saved character data.
+     */
     public static void displayCard(Player player, Player targetPlayer){
-        PlayerProfile targetPlayerProfile = CharacterCards.playerProfiles.get(targetPlayer.getUniqueId());
-
-        StringBuilder dashDecor = new StringBuilder();
-        int nameLength;
-        nameLength = targetPlayer.getName().length() / 2;
-
-        dashDecor.append("=".repeat(Math.max(0, 16 - nameLength)));
-
-        player.sendMessage(ColorParser.of("<dark_gray>╔<b>" + dashDecor + "</b></dark_gray><gray>[<dark_aqua>" + targetPlayer.getName() + "’s Card</dark_aqua><gray>]<dark_gray><b>" + dashDecor + "</b>╗").build());
-        player.sendMessage(ColorParser.of("<dark_aqua>   " + Translation.of("cards.player.ign", "IGN ") + " <dark_gray>≫ <aqua>" + targetPlayer.getName()).build());
-
-        StringBuilder name = new StringBuilder();
-
-        if(!targetPlayerProfile.getCharacter_title().isEmpty())
-            name.append(targetPlayerProfile.getCharacter_title()).append(" ");
-
-        if(!targetPlayerProfile.getCharacter_first_name().isEmpty())
-            name.append(targetPlayerProfile.getCharacter_first_name()).append(" ");
-
-        if(!targetPlayerProfile.getCharacter_last_name().isEmpty())
-            name.append(targetPlayerProfile.getCharacter_last_name()).append(" ");
-
-        if(!targetPlayerProfile.getCharacter_suffix().isEmpty())
-            name.append(targetPlayerProfile.getCharacter_suffix()).append(" ");
-
-        player.sendMessage(ColorParser.of("<dark_aqua>   " + Translation.of("cards.player.name", "Name") + " <dark_gray>≫ <aqua>" + name).build());
-        player.sendMessage(ColorParser.of("<dark_aqua>   " + Translation.of("cards.player.gender", "Gender") + " <dark_gray>≫ <aqua>" + targetPlayerProfile.getCharacter_gender()).build());
-        player.sendMessage(ColorParser.of("<dark_aqua>   " + Translation.of("cards.player.age", "Age") + " <dark_gray>≫ <aqua>" + targetPlayerProfile.getCharacter_age()).build());
-
-        if(!Settings.getRawCustomFields().isEmpty()) {
-            for (Map.Entry<String, String> entry : Settings.getRawCustomFields().entrySet()) {
-                String fieldLabel = entry.getKey();
-                String fieldPlaceholder = PlaceholderAPI.setPlaceholders(targetPlayer, entry.getValue());
-                player.sendMessage(ColorParser.of("<dark_aqua>   " + fieldLabel + " <dark_gray>≫ <aqua>" + fieldPlaceholder).build());
-            }
-        }
-
-        player.sendMessage(ColorParser.of("<dark_gray><b> <st>==============</st></b><gray>[<dark_aqua>" + Translation.of("cards.player.description", "Description") +"<gray>]<b><dark_gray><st>==============").build());
-        player.sendMessage(ColorParser.of("<aqua> " + targetPlayerProfile.getCharacter_description()).build());
-        player.sendMessage(ColorParser.of("<dark_gray>╚<b>===============</b><dark_aqua>≪ °❈° ≫<dark_gray><b>================</b>╝").build());
+        displayGenericCard(player, targetPlayer, null);
     }
 
-    @SuppressWarnings({"all"})
+    /**
+     * Displays the character card of an offline player to a player, showing all saved character data.
+     */
     public static void displayOfflinePlayerCard(Player player, OfflinePlayer targetPlayer){
-        PlayerProfile targetPlayerProfile = CharacterCards.playerProfiles.get(targetPlayer.getUniqueId());
-
-        StringBuilder dashDecor = new StringBuilder();
-        int nameLength;
-        nameLength = targetPlayer.getPlayer().getName().length() / 2;
-
-        dashDecor.append("=".repeat(Math.max(0, 16 - nameLength)));
-
-        player.sendMessage(ColorParser.of("<dark_gray>╔<b>" + dashDecor + "</b></dark_gray><gray>[<dark_aqua>" + targetPlayer.getName() + "’s Card</dark_aqua><gray>]<dark_gray><b>" + dashDecor + "</b>╗").build());
-        player.sendMessage(ColorParser.of("<dark_aqua>   " + Translation.of("cards.player.lastonline", "Last Online") + " <dark_gray>≫ <aqua>" +
-            Instant.ofEpochMilli(targetPlayer.getLastSeen()).atZone(ZoneId.of("America/New_York")).format(DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm:ss a z"))
-        ).build());
-        player.sendMessage(ColorParser.of("<dark_aqua>   " + Translation.of("cards.player.ign", "IGN ") + " <dark_gray>≫ <aqua>" + targetPlayer.getName()).build());
-
-        StringBuilder name = new StringBuilder();
-
-        if(!targetPlayerProfile.getCharacter_title().isEmpty())
-            name.append(targetPlayerProfile.getCharacter_title()).append(" ");
-
-        if(!targetPlayerProfile.getCharacter_first_name().isEmpty())
-            name.append(targetPlayerProfile.getCharacter_first_name()).append(" ");
-
-        if(!targetPlayerProfile.getCharacter_last_name().isEmpty())
-            name.append(targetPlayerProfile.getCharacter_last_name()).append(" ");
-
-        if(!targetPlayerProfile.getCharacter_suffix().isEmpty())
-            name.append(targetPlayerProfile.getCharacter_suffix()).append(" ");
-
-        player.sendMessage(ColorParser.of("<dark_aqua>   " + Translation.of("cards.player.name", "Name") + " <dark_gray>≫ <aqua>" + name).build());
-        player.sendMessage(ColorParser.of("<dark_aqua>   " + Translation.of("cards.player.gender", "Gender") + " <dark_gray>≫ <aqua>" + targetPlayerProfile.getCharacter_gender()).build());
-        player.sendMessage(ColorParser.of("<dark_aqua>   " + Translation.of("cards.player.age", "Age") + " <dark_gray>≫ <aqua>" + targetPlayerProfile.getCharacter_age()).build());
-
-        if(!Settings.getRawCustomFields().isEmpty()) {
-            for (Map.Entry<String, String> entry : Settings.getRawCustomFields().entrySet()) {
-                String fieldLabel = entry.getKey();
-                String fieldPlaceholder = PlaceholderAPI.setPlaceholders(targetPlayer, entry.getValue());
-                player.sendMessage(ColorParser.of("<dark_aqua>   " + fieldLabel + " <dark_gray>≫ <aqua>" + fieldPlaceholder).build());
-            }
-        }
-
-        player.sendMessage(ColorParser.of("<dark_gray><b> <st>==============</st></b><gray>[<dark_aqua>" + Translation.of("cards.player.description", "Description") +"<gray>]<b><dark_gray><st>==============").build());
-        player.sendMessage(ColorParser.of("<aqua> " + targetPlayerProfile.getCharacter_description()).build());
-        player.sendMessage(ColorParser.of("<dark_gray>╚<b>===============</b><dark_aqua>≪ °❈° ≫<dark_gray><b>================</b>╝").build());
+        displayGenericCard(player, targetPlayer, Instant.ofEpochMilli(targetPlayer.getLastSeen()));
     }
 
-    public static void displayHelp(Player player) {
-        player.sendMessage(ColorParser.of("<dark_gray>╔<b><st>===========================================</st></b>╗").build());
-        player.sendMessage(ColorParser.of("                         <dark_aqua>CharacterCards <aqua><b>" + CharacterCards.getInstance().getPluginMeta().getVersion().substring(0, 5)).build());
-        player.sendMessage(ColorParser.of("<dark_gray>╚<b><st>===========================================</st></b>╝").build());
-        player.sendMessage(ColorParser.of("  <gray>/<dark_aqua>char edit <aqua>title <gray><<aqua>Title<gray>> <dark_gray>- <white>" + Translation.of("cards.help.title", "Set your character's title")).build());
-        player.sendMessage(ColorParser.of("  <gray>/<dark_aqua>char edit <aqua>firstname <gray><<aqua>First Name<gray>> <dark_gray>- <white>" + Translation.of("cards.help.firstname", "Set your character's first name")).build());
-        player.sendMessage(ColorParser.of("  <gray>/<dark_aqua>char edit <aqua>lastname <gray><<aqua>Last Name<gray>> <dark_gray>- <white>" + Translation.of("cards.help.lastname", "Set your character's last name")).build());
-        player.sendMessage(ColorParser.of("  <gray>/<dark_aqua>char edit <aqua>suffix <gray><<aqua>Suffix<gray>> <dark_gray>- <white>" + Translation.of("cards.help.suffix", "Set your character's suffix")).build());
-        player.sendMessage(ColorParser.of("  <gray>/<dark_aqua>char edit <aqua>gender <gray><<aqua>Gender<gray>> <dark_gray>- <white>" + Translation.of("cards.help.gender", "Set your character's gender")).build());
-        player.sendMessage(ColorParser.of("  <gray>/<dark_aqua>char edit <aqua>age <gray><<aqua>" + Settings.getMinimumAge() + "<gray>-<aqua>"
-            + Settings.getMaximumAge() + "<gray>> <dark_gray>- <white>" + Translation.of("cards.help.age", "Set your character's age.")).build());
-        player.sendMessage(ColorParser.of("  <gray>/<dark_aqua>char edit <aqua>desc <gray><<aqua>Description<gray>> <dark_gray>- <white>" + Translation.of("cards.help.desc", "Set your character's description, <red><b>512 CHARACTER LIMIT")).build());
-        player.sendMessage(ColorParser.of("  <gray>/<dark_aqua>char help <dark_gray>- <white>" + Translation.of("cards.help.help", "Displays the plugin commands")).build());
-        player.sendMessage(ColorParser.of("  <gray>/<dark_aqua>char view <gray>{<aqua>Player<gray>} <dark_gray>- <white>" + Translation.of("cards.help.view", "View your card, or another player's")).build());
-        player.sendMessage(ColorParser.of("  <gray>/<dark_aqua>char viewoffline <gray>{<aqua>Player<gray>} <dark_gray>- <white>" + Translation.of("cards.help.viewoffline", "Views an offline player's card")).build());
+    /**
+     * Displays all relevant CharacterCards commands to a player.
+     */
+    public static void displayHelp(Player viewer) {
+        sendFormattedMessage(viewer, "<dark_gray>╔<b><st>===========================================</st></b>╗");
+        sendFormattedMessage(viewer, "                         <dark_aqua>CharacterCards <aqua><b>" + CharacterCards.getInstance().getPluginMeta().getVersion().substring(0, 5));
+        sendFormattedMessage(viewer, "<dark_gray>╚<b><st>===========================================</st></b>╝");
+        sendFormattedMessage(viewer, "  <gray>/<dark_aqua>char edit <aqua>title <gray><<aqua>Title<gray>> <dark_gray>- <white>" + Translation.of("cards.help.title", "Set your character's title"));
+        sendFormattedMessage(viewer, "  <gray>/<dark_aqua>char edit <aqua>firstname <gray><<aqua>First Name<gray>> <dark_gray>- <white>" + Translation.of("cards.help.firstname", "Set your character's first name"));
+        sendFormattedMessage(viewer, "  <gray>/<dark_aqua>char edit <aqua>lastname <gray><<aqua>Last Name<gray>> <dark_gray>- <white>" + Translation.of("cards.help.lastname", "Set your character's last name"));
+        sendFormattedMessage(viewer, "  <gray>/<dark_aqua>char edit <aqua>suffix <gray><<aqua>Suffix<gray>> <dark_gray>- <white>" + Translation.of("cards.help.suffix", "Set your character's suffix"));
+        sendFormattedMessage(viewer, "  <gray>/<dark_aqua>char edit <aqua>gender <gray><<aqua>Gender<gray>> <dark_gray>- <white>" + Translation.of("cards.help.gender", "Set your character's gender"));
+        sendFormattedMessage(viewer, "  <gray>/<dark_aqua>char edit <aqua>age <gray><<aqua>" + Settings.getMinimumAge() + "<gray>-<aqua>"
+            + Settings.getMaximumAge() + "<gray>> <dark_gray>- <white>" + Translation.of("cards.help.age", "Set your character's age."));
+        sendFormattedMessage(viewer, "  <gray>/<dark_aqua>char edit <aqua>desc <gray><<aqua>Description<gray>> <dark_gray>- <white>" + Translation.of("cards.help.desc", "Set your character's description, <red><b>512 CHARACTER LIMIT"));
+        sendFormattedMessage(viewer, "  <gray>/<dark_aqua>char help <dark_gray>- <white>" + Translation.of("cards.help.help", "Displays the plugin commands"));
+        sendFormattedMessage(viewer, "  <gray>/<dark_aqua>char view <gray>{<aqua>Player<gray>} <dark_gray>- <white>" + Translation.of("cards.help.view", "View your card, or another player's"));
+        sendFormattedMessage(viewer, "  <gray>/<dark_aqua>char viewoffline <gray>{<aqua>Player<gray>} <dark_gray>- <white>" + Translation.of("cards.help.viewoffline", "Views an offline player's card"));
+    }
+
+    private static void displayGenericCard(Player viewer, OfflinePlayer targetPlayer, Instant lastSeen) {
+        if(targetPlayer == null) return;
+
+        String formattedLastSeen = lastSeen != null
+            ? lastSeen.atZone(DEFAULT_ZONE).format(DEFAULT_TIME_FORMAT)
+            : null;
+
+        String targetName = targetPlayer.getName();
+
+        PlayerProfile targetPlayerProfile = CharacterCards.playerProfiles.get(targetPlayer.getUniqueId());
+        if (targetPlayerProfile == null) {
+            sendFormattedMessage(viewer, "<red>Player profile not found.");
+            return;
+        }
+
+        String dashDecor = getDashPaddingForName(targetPlayer);
+
+        sendFormattedMessage(viewer, "<dark_gray>╔<b>" + dashDecor + "</b></dark_gray><gray>[<dark_aqua>" + targetName + "’s Card</dark_aqua><gray>]<dark_gray><b>" + dashDecor + "</b>╗");
+        if (formattedLastSeen != null) {
+            sendFormattedMessage(viewer, "<dark_aqua>   " + Translation.of("cards.player.lastonline", "Last Online") + " <dark_gray>≫ <aqua>" + formattedLastSeen);
+        }
+        sendFormattedMessage(viewer, "<dark_aqua>   " + Translation.of("cards.player.ign", "IGN ") + " <dark_gray>≫ <aqua>" + targetName);
+        sendFormattedMessage(viewer, "<dark_aqua>   " + Translation.of("cards.player.name", "Name") + " <dark_gray>≫ <aqua>" + returnFullName(targetPlayerProfile));
+        sendFormattedMessage(viewer, "<dark_aqua>   " + Translation.of("cards.player.gender", "Gender") + " <dark_gray>≫ <aqua>" + targetPlayerProfile.getCharacter_gender());
+        sendFormattedMessage(viewer, "<dark_aqua>   " + Translation.of("cards.player.age", "Age") + " <dark_gray>≫ <aqua>" + targetPlayerProfile.getCharacter_age());
+        displayCustomFields(viewer, targetPlayer);
+        sendFormattedMessage(viewer, "<dark_gray><b> <st>==============</st></b><gray>[<dark_aqua>" + Translation.of("cards.player.description", "Description") +"<gray>]<b><dark_gray><st>==============");
+        sendFormattedMessage(viewer, "<aqua> " + targetPlayerProfile.getCharacter_description());
+        sendFormattedMessage(viewer, "<dark_gray>╚<b>===============</b><dark_aqua>≪ °❈° ≫<dark_gray><b>================</b>╝");
+    }
+
+    private static void sendFormattedMessage(Player viewer, String string) {
+        viewer.sendMessage(ColorParser.of(string).build());
+    }
+
+    private static String returnFullName(PlayerProfile profile) {
+        String[] profileNameParts = {
+            profile.getCharacter_title(),
+            profile.getCharacter_first_name(),
+            profile.getCharacter_last_name(),
+            profile.getCharacter_suffix()
+        };
+
+        return Arrays.stream(profileNameParts)
+            .filter(part -> part != null && !part.isEmpty())
+            .collect(Collectors.joining(" "));
+    }
+
+    private static void displayCustomFields(Player viewer, OfflinePlayer targetPlayer) {
+        Settings.getRawCustomFields().forEach((fieldLabel, placeholder) -> {
+            String fieldPlaceholder = PlaceholderAPI.setPlaceholders(targetPlayer, placeholder);
+            sendFormattedMessage(viewer, "<dark_aqua>   " + fieldLabel + " <dark_gray>≫ <aqua>" + fieldPlaceholder);
+        });
+    }
+
+    private static String getDashPaddingForName(OfflinePlayer targetPlayer) {
+        if(targetPlayer.getName() == null) return "===============";
+
+        int nameLength = targetPlayer.getName().length();
+        int dashCount = Math.max(0, 16 - nameLength / 2);
+
+        return "=".repeat(dashCount);
     }
 }
