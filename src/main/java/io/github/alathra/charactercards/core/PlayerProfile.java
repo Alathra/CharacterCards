ba50b7f19d35a3a66f537b9b71cc4f47f8623391
Schbem <131344591+Schbem@users.noreply.github.com>
2025-04-14 19:00:07 +0800

    refactor: refactored PlayerProfile.java

--- src/main/java/io/github/alathra/charactercards/core/PlayerProfile.java
+++ src/main/java/io/github/alathra/charactercards/core/PlayerProfile.java
@@ -3,59 +3,87 @@ package io.github.alathra.charactercards.core;
 import java.util.UUID;
 
 public class PlayerProfile implements Cloneable{
-    private final UUID player_uuid;
-    private String player_name;
-    private String character_title;
-    private String character_first_name;
-    private String character_last_name;
-    private String character_suffix;
-    private String character_gender;
-    private int character_age;
-    private String character_description;
+    private final UUID playerUuid;
+    private String playerName;
+    private String characterTitle;
+    private String characterFirstName;
+    private String characterLastName;
+    private String characterSuffix;
+    private String characterGender;
+    private int characterAge;
+    private String characterDescription;
 
 
     public PlayerProfile(UUID playerUuid, String playerName, String characterTitle, String characterFirstName, String characterLastName, String characterSuffix, String characterGender, int characterAge, String characterDescription) {
-        player_uuid = playerUuid;
-        player_name = playerName;
-        character_title = characterTitle;
-        character_first_name = characterFirstName;
-        character_last_name = characterLastName;
-        character_suffix = characterSuffix;
-        character_gender = characterGender;
-        character_age = characterAge;
-        character_description = characterDescription;
+        this.playerUuid = playerUuid;
+        this.playerName = playerName;
+        this.characterTitle = characterTitle;
+        this.characterFirstName = characterFirstName;
+        this.characterLastName = characterLastName;
+        this.characterSuffix = characterSuffix;
+        this.characterGender = characterGender;
+        this.characterAge = characterAge;
+        this.characterDescription = characterDescription;
+    }
+
+    public UUID getPlayer_uuid() {
+        return playerUuid;
     }
 
     public String getPlayer_name() {
-        return player_name;
+        return playerName;
+    }
+    public void setPlayer_name(String playerName) {
+        this.playerName = playerName;
     }
 
-    public void setPlayer_name(String player_name) {
-        this.player_name = player_name;
+    public String getCharacter_title() {
+        return characterTitle;
+    }
+    public void setCharacter_title(String characterTitle) {
+        this.characterTitle = characterTitle;
+    }
+
+    public String getCharacter_first_name() {
+        return characterFirstName;
+    }
+    public void setCharacter_first_name(String characterFirstName) {
+        this.characterFirstName = characterFirstName;
+    }
+
+    public String getCharacter_last_name() {
+        return characterLastName;
+    }
+    public void setCharacter_last_name(String characterLastName) {
+        this.characterLastName = characterLastName;
+    }
+
+    public String getCharacter_suffix() {
+        return characterSuffix;
+    }
+    public void setCharacter_suffix(String characterSuffix) {
+        this.characterSuffix = characterSuffix;
     }
 
     public String getCharacter_gender() {
-        return character_gender;
+        return characterGender;
     }
-
-    public void setCharacter_gender(String character_gender) {
-        this.character_gender = character_gender;
+    public void setCharacter_gender(String characterGender) {
+        this.characterGender = characterGender;
     }
 
     public int getCharacter_age() {
-        return character_age;
+        return characterAge;
     }
-
-    public void setCharacter_age(int character_age) {
-        this.character_age = character_age;
+    public void setCharacter_age(int characterAge) {
+        this.characterAge = characterAge;
     }
 
     public String getCharacter_description() {
-        return character_description;
+        return characterDescription;
     }
-
-    public void setCharacter_description(String character_description) {
-        this.character_description = character_description;
+    public void setCharacter_description(String characterDescription) {
+        this.characterDescription = characterDescription;
     }
 
     @Override
@@ -63,43 +91,7 @@ public class PlayerProfile implements Cloneable{
         try {
             return (PlayerProfile) super.clone();
         } catch (CloneNotSupportedException e) {
-            throw new AssertionError();
+            throw new AssertionError("Cloning not supported", e);
         }
-    }
-
-    public UUID getPlayer_uuid() {
-        return player_uuid;
-    }
-
-    public String getCharacter_title() {
-        return character_title;
-    }
-
-    public void setCharacter_title(String character_title) {
-        this.character_title = character_title;
-    }
-
-    public String getCharacter_first_name() {
-        return character_first_name;
-    }
-
-    public void setCharacter_first_name(String character_first_name) {
-        this.character_first_name = character_first_name;
-    }
-
-    public String getCharacter_last_name() {
-        return character_last_name;
-    }
-
-    public void setCharacter_last_name(String character_last_name) {
-        this.character_last_name = character_last_name;
-    }
-
-    public String getCharacter_suffix() {
-        return character_suffix;
-    }
-
-    public void setCharacter_suffix(String character_suffix) {
-        this.character_suffix = character_suffix;
     }
 }
